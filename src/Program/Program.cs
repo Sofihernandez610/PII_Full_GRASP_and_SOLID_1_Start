22eb10cddda4036c808c1a5d00246af3e77e25c0
Sofi Hernandez <sofihernandez@192.168.0.115>
2022-09-01 12:44:52 -0300

    terminado

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -26,6 +26,9 @@ namespace Full_GRASP_And_SOLID
             recipe.AddStep(new Step(GetProduct("Café"), 100, GetEquipment("Cafetera"), 120));
             recipe.AddStep(new Step(GetProduct("Leche"), 200, GetEquipment("Hervidor"), 60));
             recipe.PrintRecipe();
+            double insumos= recipe.Costoinsumos();
+            double equipamiento = recipe.Costoequip();
+            recipe.GetProductionCost(insumos,equipamiento);
         }
 
         private static void PopulateCatalogs()
