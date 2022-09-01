74ca794ecbc1b8ce87bcf334329d4937472c2842
Sofi Hernandez <sofihernandez@192.168.0.115>
2022-09-01 12:50:15 -0300

    h

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -34,5 +34,45 @@ namespace Full_GRASP_And_SOLID.Library
                     $"usando '{step.Equipment.Description}' durante {step.Time}");
             }
         }
+        public double Costoinsumos () //metodo para calcular la sumatoria de los costos de los productos
+        {
+            double costoinsumos=0;
+            foreach (Step step in this.steps)
+            {
+
+            costoinsumos = costoinsumos + step.Input.UnitCost;
+            
+            }
+            Console.WriteLine($"El costo de los insumos fue de: {costoinsumos} ");
+            return costoinsumos;
+        }
+ //Costo equipamiento = Sumatoria de tiempo de uso 
+        //                     x costo/hora del equipo para todos los pasos de la receta
+        //Asumo que el tiempo esta en segundos
+        public double Costoequip() 
+        {
+            double costoequip=0;
+             foreach (Step step in this.steps)
+            {
+            double segundos = step.Time;
+            double divisor = 3600;
+            double horas = segundos/divisor;
+            costoequip = costoequip + (step.Equipment.HourlyCost*horas);
+            
+            }
+            Console.WriteLine($"El costo del equipamiento fue de: {costoequip} ");
+            return costoequip;
+
+        }
+
+        public void GetProductionCost(double costoinsumos, double costoequip)
+        {
+            double Costototal = costoequip + costoinsumos ;
+            Console.WriteLine($"El costo total fue de : {Costototal}");
+            
+        }
+
+
+        
     }
 }
